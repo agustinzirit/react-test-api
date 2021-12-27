44b50f224a5a8ae492ab2bd045744326e4723193
Agustin Marcano <agustinzirit@gmail.com>
2021-12-27 02:20:57 -0400

    apply standard

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// app.post('/api/search', (req, res) => {
-//   console.log(req.body.text)
-//   res.json({ text: req.body.text })
-// })
-
 app.use('/api/search', searchRoute)
 
 app.listen(3000, () => {
--- src/tests/query.test.js
+++ src/tests/query.test.js
@@ -1,51 +1,50 @@
-const request = require("supertest")("http://localhost:3000/api");
-const expect = require("chai").expect;
+const request = require('supertest')('http://localhost:3000/api')
+const expect = require('chai').expect
 
-describe("GET /search?text= (Without param)", () => {
-  it("return words reverse - Failed", async () => {
-    const response = await request.get("/search");
+describe('GET /search?text= (Without param)', () => {
+  it('return words reverse - Failed', async () => {
+    const response = await request.get('/search')
 
-    expect(response.status).to.eql(400);
-  });
-});
+    expect(response.status).to.eql(400)
+  })
+})
 
-describe("GET /search?text=bear (with param)", () => {
-  it("return words reverse - Success", async () => {
-    const response = await request.get("/search/bear");
+describe('GET /search?text=bear (with param)', () => {
+  it('return words reverse - Success', async () => {
+    const response = await request.get('/search/bear')
 
-    expect(response.status).to.eql(200);
-    expect(response.body).to.eql({ text: "raeb", palindrome: false });
-  });
+    expect(response.status).to.eql(200)
+    expect(response.body).to.eql({ text: 'raeb', palindrome: false })
+  })
 
-  it("bear is not palindrome", async () => {
-    const response = await request.get("/search/bear");
+  it('bear is not palindrome', async () => {
+    const response = await request.get('/search/bear')
 
-    expect(response.body.palindrome).to.eql(false);
-    expect(response.body.text).to.eql("raeb");
+    expect(response.body.palindrome).to.eql(false)
+    expect(response.body.text).to.eql('raeb')
+  })
+})
 
-  });
-});
+describe('GET /search?text=dad (with param)', () => {
+  it('return words reverse - Success', async () => {
+    const response = await request.get('/search/dad')
 
-describe("GET /search?text=dad (with param)", () => {
-  it("return words reverse - Success", async () => {
-    const response = await request.get("/search/dad");
+    expect(response.status).to.eql(200)
+    expect(response.body).to.eql({ text: 'dad', palindrome: true })
+    expect(response.body.text).to.eql('dad')
+  })
 
-    expect(response.status).to.eql(200);
-    expect(response.body).to.eql({ text: "dad", palindrome: true });
-    expect(response.body.text).to.eql("dad");
-  });
+  it('dad is palindrome', async () => {
+    const response = await request.get('/search/dad')
 
-  it("dad is palindrome", async () => {
-    const response = await request.get("/search/dad");
+    expect(response.body.palindrome).to.eql(true)
+  })
+})
 
-    expect(response.body.palindrome).to.eql(true);
-  });
-});
+describe('GET /list (without param)', function () {
+  it('return list', async function () {
+    const response = await request.get('/list')
 
-describe("GET /list (without param)", function () {
-  it("return list", async function () {
-    const response = await request.get("/list");
-
-    expect(response.status).to.eql(404);
-  });
-});
+    expect(response.status).to.eql(404)
+  })
+})
